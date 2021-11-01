ef36a75bf0eed248845520dfdee8a2d581b72973
ShamSandra <79218160+ShamSandra@users.noreply.github.com>
2021-10-31 22:03:16 -0400

    Create vaccination.java

---
+++ vaccination.java
@@ -0,0 +1,13 @@
+package vaccination;
+
+import addresses.CivicAddress;
+import addresses.EmailAddress;
+import addresses.OnlineAddress;
+
+public class Clinic {
+	//were going to use composition to access all the available date in patient, vaccine, and addresses class
+	private CivicAddress civicaddress;
+	private EmailAddress emailAddress;
+	private OnlineAddress onlineAddress;
+
+}
